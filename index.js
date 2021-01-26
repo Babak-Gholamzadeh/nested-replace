63b4a8e3d35e9907b562af1741a78273d9d2eba5
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-01-26 15:55:01 +0330

    implement the functionality

---
+++ index.js
@@ -0,0 +1,23 @@
+const realTypeOf = require('./realtypeof');
+
+const nestedReplace = (input, findValue, replaceValue) => {
+  if (realTypeOf.isString(input))
+    return input
+      .replace(findValue, replaceValue);
+
+  else if (realTypeOf.isArray(input))
+    return input
+      .map(value => nestedReplace(value, findValue, replaceValue));
+
+  else if (realTypeOf.isObject(input))
+    return Object
+      .entries(input)
+      .reduce((acc, [key, value]) => {
+        acc[key] = nestedReplace(value, findValue, replaceValue);
+        return acc;
+      }, {});
+
+  return input;
+};
+
+module.exports = nestedReplace;
